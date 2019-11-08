3ee389c6238862db32162a9431594cc4fc3794cd
Reynaldi Oktino <reynaldiolp@gmail.com>
2019-11-08 16:14:20 +0700

    Tugas Android

---
+++ app/src/main/java/com/example/karyaseni/Login.java
@@ -0,0 +1,49 @@
+package com.example.karyaseni;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class Login extends AppCompatActivity {
+
+    Button login;
+    TextView register, forgot;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        login = findViewById(R.id.btnLogin);
+        register = findViewById(R.id.txtSignUpHere);
+        forgot = findViewById(R.id.txtForgotPassword);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Login.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Login.this, Register.class);
+                startActivity(intent);
+            }
+        });
+
+        forgot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Login.this, Forgot.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
